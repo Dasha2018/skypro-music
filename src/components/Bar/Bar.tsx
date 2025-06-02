b5aee69112e535ca5d4073263bb65010ae99b0ef
Dasha2018 <zavadskayadasha97@gmail.com>
2025-06-02 21:06:41 +0700

    Реализовала воспроизведение треков, оживила проигрыватель

--- src/components/Bar/Bar.tsx
+++ src/components/Bar/Bar.tsx
@@ -1,10 +1,55 @@
+'use client';
 import Link from 'next/link';
 import styles from './bar.module.css';
 import classnames from 'classnames';
+import { useAppDispatch, useAppSelector } from '@/store/store';
+import { useRef, useEffect } from 'react';
+import { setIsPlay } from '@/store/features/trackSlice';
 
 export default function Bar() {
+  const currentTrack = useAppSelector((state) => state.tracks.currentTrack);
+  const isPlay = useAppSelector((state) => state.tracks.isPlay);
+  const dispatch = useAppDispatch();
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+  useEffect(() => {
+    if (audioRef.current && currentTrack) {
+      audioRef.current.load(); // Перезагружаем аудио
+      if (isPlay) {
+        audioRef.current.play();
+      }
+    }
+  }, [currentTrack, isPlay]);
+
+  if (!currentTrack) return <></>;
+
+  const togglePlay = () => {
+    if (!audioRef.current) return;
+
+    if (isPlay) {
+      audioRef.current.pause();
+      dispatch(setIsPlay(false));
+    } else {
+      audioRef.current.play();
+      dispatch(setIsPlay(true));
+    }
+  };
+
+  /*   const playTrack = () => {
+    if (audioRef.current) {
+      audioRef.current.play();
+      dispatch(setIsPlay(true));
+    }
+  };
+  const pauseTrack = () => {
+    if (audioRef.current) {
+      audioRef.current.pause();
+      dispatch(setIsPlay(false));
+    }
+  }; */
+
   return (
     <div className={styles.bar}>
+      <audio ref={audioRef} src={currentTrack?.track_file}></audio>
       <div className={styles.bar__content}>
         <div className={styles.bar__playerProgress}></div>
         <div className={styles.bar__playerBlock}>
@@ -15,9 +60,18 @@ export default function Bar() {
                   <use xlinkHref="/img/icon/sprite.svg#icon-prev"></use>
                 </svg>
               </div>
-              <div className={classnames(styles.player__btnPlay, styles.btn)}>
+              <div
+                className={classnames(styles.player__btnPlay, styles.btn)}
+                onClick={togglePlay}
+              >
                 <svg className={styles.player__btnPlaySvg}>
-                  <use xlinkHref="/img/icon/sprite.svg#icon-play"></use>
+                  <use
+                    xlinkHref={
+                      isPlay
+                        ? '/img/icon/sprite.svg#icon-pause'
+                        : '/img/icon/sprite.svg#icon-play'
+                    }
+                  ></use>
                 </svg>
               </div>
               <div className={styles.player__btnNext}>
@@ -53,12 +107,12 @@ export default function Bar() {
                 </div>
                 <div className={styles.trackPlay__author}>
                   <Link className={styles.trackPlay__authorLink} href="">
-                    Ты та...
+                    {currentTrack?.name || 'Неизвестный трек'}
                   </Link>
                 </div>
                 <div className={styles.trackPlay__album}>
                   <Link className={styles.trackPlay__albumLink} href="">
-                    Баста
+                    {currentTrack?.author || 'Неизвестный исполнитель'}
                   </Link>
                 </div>
               </div>
@@ -102,6 +156,15 @@ export default function Bar() {
                   )}
                   type="range"
                   name="range"
+                  min={0}
+                  max={1}
+                  step={0.01}
+                  defaultValue={1}
+                  onChange={(e) => {
+                    if (audioRef.current) {
+                      audioRef.current.volume = parseFloat(e.target.value);
+                    }
+                  }}
                 />
               </div>
             </div>
